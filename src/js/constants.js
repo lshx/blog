e487362053760b4e7e378914213a049f67681f83
李盛玺 <361819668@qq.com>
2017-09-15 15:52:11 +0800

    Create constants.js

---
+++ src/js/constants.js
@@ -0,0 +1,41 @@
+/**
+ * Created by lishengxi on 2017/9/15.
+ */
+var Constants = Constants || {};
+Constants.url = "/";
+Constants.TAG = [
+    {
+        name: "Java",
+        remark: "Java是一种简单的，跨平台的，面向对象的，分布式的，解释的，健壮的安全的，结构的中立的，可移植的，性能很优异的多线程的，动态的语言。"
+    },
+    {
+        name: "Restful",
+        remark: "一种软件架构风格，设计风格而不是标准，只是提供了一组设计原则和约束条件。主要用于http调用"
+    },
+    {
+        name: "Protocol",
+        remark: "协议"
+    }
+];
+Constants.TAG = [
+    {
+        name: "HikVision",
+        remark: "海康威视"
+    },
+    {
+        name: "AsiaInfo",
+        remark: "亚信"
+    }
+];
+Constants.PAGE = [
+    {
+        id: "1505455961941",
+        date: "2017-09-15",
+        type: "js",
+        title: "HttpClient Fluent 链式调用Restful接口",
+        remark: "借用链式调用方式，大大优化了Java调用Http接口的代码量，简单优雅。",
+        url: Constants.url + 'src/html/httpClientFluent.html',
+        tag: ["Java", "Restful", "Protocol"],
+        company: 'HikVision'
+    }
+];
